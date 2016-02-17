ad4fa64a045a9011b7937265b58780c79e9fdc34
davit <davstepanyan95@gmail.com>
2016-02-17 16:55:18 +0400

    with int mr_id

--- app/Services/CenterService.php
+++ app/Services/CenterService.php
@@ -71,7 +71,7 @@ class CenterService {
 	 */
 	public function getMeetingRoomPrice($center_id, $mr_id) {
 		$center = $this->center->where('id', $center_id)->first();
-		return $meeting_rooms = $center->meeting_rooms->where('id', $mr_id)->first()->hourly_rate;
+		return $meeting_rooms = $center->meeting_rooms->where('id', (int)$mr_id)->first()->hourly_rate;
 	}
 
 
