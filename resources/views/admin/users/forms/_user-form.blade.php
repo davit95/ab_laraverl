4b527b54c056545fbfa9cd13db7cec9da9307d5c
divanovphp <divanovphp@gmail.com>
2016-05-03 19:33:32 +0400

    users blade bags

--- resources/views/admin/users/forms/_user-form.blade.php
+++ resources/views/admin/users/forms/_user-form.blade.php
@@ -18,15 +18,15 @@
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Create A First Name</label></div>
-	    		<div class="col-md-8">{!! Form::text('first_name', null, [ 'class' => 'form-control', 'placeholder' => 'user name' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('first_name', null, [ 'class' => 'form-control', 'placeholder' => 'first name' ]) !!}</div>
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Create A Last Name</label></div>
-	    		<div class="col-md-8">{!! Form::text('last_name', null, [ 'class' => 'form-control', 'placeholder' => 'user name' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('last_name', null, [ 'class' => 'form-control', 'placeholder' => 'last name' ]) !!}</div>
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Create A Password</label></div>
-	    		<div class="col-md-8">{!! Form::text('password', null, [ 'class' => 'form-control', 'placeholder' => 'user name' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('password', null, [ 'class' => 'form-control', 'placeholder' => 'password' ]) !!}</div>
 	    	</div>
 	    </div>
 	    <div class="panel-body col-md-6">
@@ -36,19 +36,19 @@
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Phone Number:</label></div>
-	    		<div class="col-md-8">{!! Form::text('phone', null, [ 'class' => 'form-control', 'placeholder' => 'Owner\'s Email' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('phone', null, [ 'class' => 'form-control', 'placeholder' => 'phone' ]) !!}</div>
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Address1:</label></div>
-	    		<div class="col-md-8">{!! Form::text('address1', null, [ 'class' => 'form-control', 'placeholder' => 'Owner\'s Email' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('address1', null, [ 'class' => 'form-control', 'placeholder' => 'address1' ]) !!}</div>
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Address2:</label></div>
-	    		<div class="col-md-8">{!! Form::text('address2', null, [ 'class' => 'form-control', 'placeholder' => 'Owner\'s Email' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('address2', null, [ 'class' => 'form-control', 'placeholder' => 'address2' ]) !!}</div>
 	    	</div>
 	    	<div class="row form-group">
 	    		<div class="col-md-4 text-right"><label>Postal Code:</label></div>
-	    		<div class="col-md-8">{!! Form::text('postal_code', null, [ 'class' => 'form-control', 'placeholder' => 'Owner\'s Email' ]) !!}</div>
+	    		<div class="col-md-8">{!! Form::text('postal_code', null, [ 'class' => 'form-control', 'placeholder' => 'postal code' ]) !!}</div>
 	    	</div>
 	    </div>
 	    <div class="clearfix"></div>
