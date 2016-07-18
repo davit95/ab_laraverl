560876536145b4fbcb950fd8c4356bbbe8932e3c
Max <vahagn.sardaryan@gmail.com>
2016-07-18 17:53:50 +0400

    fixes

--- api/Server/Services/LocationService.php
+++ api/Server/Services/LocationService.php
@@ -436,7 +436,7 @@ class LocationService {
 				'address_1'     => $location->address1,
 				'address_2'     => $location->address2,
 				'city'          => $location->city_name,
-				'company_name'  => $location->owner->company_name,
+				'company_name'  => $location->owner_user->company_name,
 				'city_slug'     => null!= $location->city ? $location->city->slug : '',
 				'state'         => $location->us_state,
 				'postal_code'   => $location->postal_code,
