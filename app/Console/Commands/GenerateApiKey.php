e3e20889a9117d0b2f2864b48bc559f3333077ee
Max <vahagn.sardaryan@gmail.com>
2016-07-23 11:32:55 +0400

    api key

--- app/Console/Commands/GenerateApiKey.php
+++ app/Console/Commands/GenerateApiKey.php
@@ -17,7 +17,7 @@ class GenerateApiKey extends Command
      *
      * @var string
      */
-    protected $signature = "api-key";
+    protected $signature = "api-key {full_access=false}";
 
     /**
      * The console command description.
@@ -46,11 +46,12 @@ class GenerateApiKey extends Command
         $this->info('Generating new API key and secret');
         $this->info('');
         $api_key = str_random(20);
-        $api_secret = str_random(20);
-        // $ip = $this->argument('ip');
+        $api_secret = str_random(20);        
+        $full_access= $this->argument('full_access');
         $apiCred->create([
             'api_key'    => $api_key,
             'api_secret' => $api_secret,
+            'full_access' => $full_access
             // 'origin'     => $ip
         ]);
         // $this->info('New API key and secret for '.$ip);
@@ -66,8 +67,8 @@ class GenerateApiKey extends Command
      */
     protected function getArguments()
     {
-        return [
-            // ['ip', InputArgument::REQUIRED, 'Ip address'],
+        return [            
+            ['full_access', InputArgument::REQUIRED, 'Full Access'],
         ];
     }
 }
