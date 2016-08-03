c384369cfd39cefcad2edb705214a053ef67e87c
Isaac Vollaire <ivollaire@abcn.com>
2016-08-03 09:34:08 -0700

    add last name

--- resources/views/admin/csr/parts/_order-item.blade.php
+++ resources/views/admin/csr/parts/_order-item.blade.php
@@ -2,7 +2,7 @@
     <td><a  href="{{ url('invoices/'.$invoice->id) }}">{{ $invoice->id }}</a></td>    
     <td>{{$invoice->created_at}}</td>
     <td>{{$invoice->type}}</td>
-    <td><a href="{{ url('customers/'.$invoice->customer->id) }}">{{$invoice->customer->first_name}}</a></td>
+    <td><a href="{{ url('customers/'.$invoice->customer->id) }}">{{$invoice->customer->first_name}} {{$invoice->customer->last_name}}</a></td>
     <td>
     	@if(in_array($invoice->id, $new_invoices_ids))
     		new
