15eaecbff8a95a99bf6519de33870222c073c19c
divanovphp <divanovphp@gmail.com>
2016-08-25 17:16:29 +0400

    user role bug

--- api/Server/Http/Controllers/UsersController.php
+++ api/Server/Http/Controllers/UsersController.php
@@ -42,7 +42,10 @@ class UsersController extends Controller
 
     public function getUserById($id, UserService $userService) {
         $user = $userService->getUserById($id);
-        $user_role = $user->role->name;
+        $user_role = '';
+        if($user->role) {
+            $user_role = $user->role->name;
+        }
         return response()->json(['user' => $user, 'user_role' => $user_role]);
     }
 }
